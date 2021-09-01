f2cc9fa48e9408f4ab89cf6e282407ede4e5eba2
Roman Kasparian <roman.kasparian@gmail.com>
2021-09-01 17:25:48 -0400

    color themes

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function App() {
     }
   }
   return (
-    <div className="app">
+    <div className="app light">
       <Panel>
         <Output text={text}></Output>
         <Keypad keypress={keypress}></Keypad>
