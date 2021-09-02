160e5862bd72a5585afa4f6a18bcd3a0b84a8904
Roman Kasparian <roman.kasparian@gmail.com>
2021-09-02 12:25:28 -0400

    refactored stateful logic from App into Panel

--- src/App.js
+++ src/App.js
@@ -1,56 +1,10 @@
 import "./assets/css/main.css";
-import Keypad from "./components/Keypad";
-import Output from "./components/Output";
 import Panel from "./components/Panel";
-import { useState } from "react";
-import evaluate from "./lib/evaluate";
 
 function App() {
-  let [text, setText] = useState("");
-
-  function keypress(key) {
-    let prev = text;
-    if (text === "NaN" || text === "Infinity" || text === "-Infinity" || text === "Err: Division by 0") {
-      prev = "";
-    }
-    switch (key) {
-      case "C":
-        setText("");
-        break;
-      case "/":
-      case "*":
-      case "+":
-        // only add an operator after a number
-        if (/\d$/.test(prev)) setText(prev + key);
-        break;
-      case "-":
-        // preceded by + change to minus
-        if (/\+$/.test(prev)) setText(prev.replace(/.$/, "-"));
-        // preceded by - change to plus
-        else if (/\d-$/.test(prev)) setText(prev.replace(/.$/, "+"));
-        // preceded by - but that is the whole string
-        else if (/^-$/.test(prev)) setText(prev.replace(/.$/, ""));
-        else setText(prev + "-");
-        break;
-      case ".":
-        // don't put two decimal points in a row
-        if (!/\.$/.test(prev)) setText(prev + key);
-        break;
-      case "=":
-        if (/^.*\d+$/.test(prev)) {
-          setText(String(evaluate(prev)));
-        }
-        break;
-      default:
-        setText(prev + key);
-    }
-  }
   return (
     <div className="app light">
-      <Panel>
-        <Output text={text}></Output>
-        <Keypad keypress={keypress}></Keypad>
-      </Panel>
+      <Panel></Panel>
     </div>
   );
 }
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,5 +1,60 @@
-function Panel({children}) {
-  return <span className="panel">{children}</span>;
+import Keypad from "./Keypad";
+import Output from "./Output";
+import { useState } from "react";
+import evaluate from "./../lib/evaluate";
+
+function Panel() {
+  let [text, setText] = useState("");
+
+  function keypress(key) {
+    let prev = text;
+    if (
+      text === "NaN" ||
+      text === "Infinity" ||
+      text === "-Infinity" ||
+      text === "Err: Division by 0"
+    ) {
+      prev = "";
+    }
+    switch (key) {
+      case "C":
+        setText("");
+        break;
+      case "/":
+      case "*":
+      case "+":
+        // only add an operator after a number
+        if (/\d$/.test(prev)) setText(prev + key);
+        break;
+      case "-":
+        // preceded by + change to minus
+        if (/\+$/.test(prev)) setText(prev.replace(/.$/, "-"));
+        // preceded by - change to plus
+        else if (/\d-$/.test(prev)) setText(prev.replace(/.$/, "+"));
+        // preceded by - but that is the whole string
+        else if (/^-$/.test(prev)) setText(prev.replace(/.$/, ""));
+        else setText(prev + "-");
+        break;
+      case ".":
+        // don't put two decimal points in a row
+        if (!/\.$/.test(prev)) setText(prev + key);
+        break;
+      case "=":
+        if (/^.*\d+$/.test(prev)) {
+          setText(String(evaluate(prev)));
+        }
+        break;
+      default:
+        setText(prev + key);
+    }
+  }
+
+  return (
+    <span className="panel">
+      <Output text={text}></Output>
+      <Keypad keypress={keypress}></Keypad>
+    </span>
+  );
 }
 
 export default Panel;
