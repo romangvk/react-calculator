71c1e52e2d92c0294816cc6d34ac45cbc5318396
Roman Kasparian <roman.kasparian@gmail.com>
2021-09-09 11:28:01 -0400

    imported changes from vue calculator

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,11 +1,39 @@
-function Keypad({keypress}) {
-  let keys = ['/', '*', '-', '7', '8', '9', '4', '5', '6', '1', '2', '3', '.'];
+function Keypad({ keypress }) {
+  let keys = [
+    "(",
+    ")",
+    "^",
+    "/",
+    "*",
+    "-",
+    "7",
+    "8",
+    "9",
+    "4",
+    "5",
+    "6",
+    "1",
+    "2",
+    "3",
+    ".",
+  ];
   return (
     <div className="container">
-      <button type="button" className="key1" onClick={() => keypress('C')}>C</button>
-      <button type="button" className="key2" onClick={() => keypress('+')}>+</button>
-      <button type="button" className="key3" onClick={() => keypress('0')}>0</button>
-      <button type="button" className="key4" onClick={() => keypress('=')}>=</button>
+      <button type="button" class="key-clear" onClick={() => keypress("C")}>
+        C
+      </button>
+      <button type="button" class="key-back" onClick={() => keypress("↩")}>
+        ↩
+      </button>
+      <button type="button" class="key-plus" onClick={() => keypress("+")}>
+        +
+      </button>
+      <button type="button" class="key-0" onClick={() => keypress("0")}>
+        0
+      </button>
+      <button type="button" class="key-equal" onClick={() => keypress("=")}>
+        =
+      </button>
       {keys.map((k, i) => (
         <button type="button" onClick={() => keypress(k)} key={i}>
           {k}
